4f02c0090434aae7b9312aa727f6cd6e6c410750
Emil Larsson <emil.larsson@tieto.com>
2020-06-12 09:53:13 +0200

    Adding poll functionality to fetchSitemap.

--- src/lib/fetchSitemap.js
+++ src/lib/fetchSitemap.js
@@ -2,13 +2,33 @@
 
 const parseXMLtoJSON = require('./parseXMLtoJSON').parseXMLtoJSON
 const convertSitemapToArrayOfUrls = require('./parseXMLtoJSON').convertSitemapToArrayOfUrls
+const poll = require('../lib/promisePoller').poll
+
 const fetch = require('node-fetch')
 
-const fetchSitemap = async url => {
-  const res = await fetch(url)
-    .then(res => res.text())
+
+const fetchSitemap = async ({ url, config }) => {
+
+  const getSitemap = () => fetch(url)
+  const validate = res => res.status === 200
+
+  let res
+
+  if (config.poll === true) {
+    res = await poll({
+      message: 'Tried to fetch Sitemap',
+      fn: getSitemap,
+      validate: validate,
+      interval: 2000,
+      maxAttempts: 2000
+    })
+    res = await res.text()
+  } else {
+    res = await getSitemap(url).then(res => res.text())
+  }
 
   const json = await parseXMLtoJSON(res)
+
   return convertSitemapToArrayOfUrls(json)
 }
 
